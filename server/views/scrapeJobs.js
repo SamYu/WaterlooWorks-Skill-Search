eb190f8c41fb3f84b02f43b75c6d5b254687dc18
Sam Yu <smyu03@gmail.com>
2020-01-08 02:57:39 -0500

    Multiple job scraper script

--- server/views/scrapeJobs.js
+++ server/views/scrapeJobs.js
@@ -1,6 +1,4 @@
-const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
-const secrets = require('../../secrets');
 
 const url = 'https://waterlooworks.uwaterloo.ca/myAccount/hire-waterloo/other-jobs/jobs-postings.htm';
 
@@ -23,46 +21,105 @@ async function auth(page, email, password) {
     await page.click(signInBtn);
 }
 
-async function run() {
+export default async function fetchLatestJobs(email, password) {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
     });
     const page = await browser.newPage();
-    await auth(page, secrets.email, secrets.password);
+    await page.setViewport({ width: 1280, height: 720 });
+
+    // Auth using email and password
+    await auth(page, email, password);
+
+    // Navigate to jobs page
     await page.waitForNavigation();
-    await page.goto(url);
-    await page.waitFor(1000);
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
     const TEMP_CLICK_LINK = '#quickSearchCountsContainer > table > tbody > tr:nth-child(5) > td.full > a';
+    await page.waitForSelector(TEMP_CLICK_LINK);
     await page.click(TEMP_CLICK_LINK);
-    await page.waitFor(1000);
     const jobCountOnPageSelector = '#totalOverAllPacks';
+    await page.waitForSelector(jobCountOnPageSelector);
+
+    // Get job count on page
     const jobCountOnPage = await page.evaluate((sel) => {
         const element = document.querySelector(sel);
         return element ? element.innerHTML.split('>')[1].split('<')[0] : 0;
     }, jobCountOnPageSelector);
+
+    // Get HTML selectors for each element
     const TABLE_SEL_PREFIX = '#postingsTable > tbody > tr:nth-child(INDEX) > ';
     const JOB_ID_SEL = `${TABLE_SEL_PREFIX}td:nth-child(3)`;
     const TITLE_SEL = `${TABLE_SEL_PREFIX}td:nth-child(4) > strong > span > a`;
+    const TITLE_SEL_ALT = `${TABLE_SEL_PREFIX}td:nth-child(4) > span:nth-child(2) > a`;
+    const TITLE_SEL_ALT2 = `${TABLE_SEL_PREFIX}td:nth-child(4) > span > a`;
     const COMPANY_SEL = `${TABLE_SEL_PREFIX}td:nth-child(5) > span`;
+    const DROPDOWN_SEL = `${TABLE_SEL_PREFIX}td:nth-child(1) > div > a`;
+    const EXPAND_JOB_SEL = `${TABLE_SEL_PREFIX}td:nth-child(1) > div > ul > li:nth-child(2) > a`;
     const jobs = [];
+
+    // Iterate through the table of jobs
     for (let i = 1; i <= jobCountOnPage; i += 1) {
+        // Get jobId, company, title
         const jobIdSelector = JOB_ID_SEL.replace('INDEX', i);
         const titleSelector = TITLE_SEL.replace('INDEX', i);
+        const titleSelectorAlt = TITLE_SEL_ALT.replace('INDEX', i);
+        const titleSelectorAlt2 = TITLE_SEL_ALT2.replace('INDEX', i);
         const companySelector = COMPANY_SEL.replace('INDEX', i);
-        const jobObj = page.evaluate((idSel, titleSel, companySel) => {
-            const jobId = document.querySelector(idSel).innerHTML;
-            const title = document.querySelector(titleSel).innerHTML.replace(/\n/g, '').replace(/\t/g, '');
-            const company = document.querySelector(companySel).innerHTML;
+        const jobObj = await page.evaluate((idSel, titleSel, titleSelAlt, titleSelAlt2, companySel) => {
+            const jobId = document.querySelector(idSel)
+                ? document.querySelector(idSel).innerHTML
+                : null;
+            let title = null;
+            if (document.querySelector(titleSel)) {
+                title = document.querySelector(titleSel).innerHTML.replace(/\n/g, '').replace(/\t/g, '');
+            } else if (document.querySelector(titleSelAlt)) {
+                title = document.querySelector(titleSelAlt).innerHTML.replace(/\n/g, '').replace(/\t/g, '');
+            } else if (document.querySelector(titleSelAlt2)) {
+                title = document.querySelector(titleSelAlt2).innerHTML.replace(/\n/g, '').replace(/\t/g, '');
+            }
+            const company = document.querySelector(companySel)
+                ? document.querySelector(companySel).innerHTML
+                : null;
             return {
                 jobId,
                 title,
                 company,
             };
-        }, jobIdSelector, titleSelector, companySelector);
-        jobs.push(jobObj);
-    }
-    const results = await Promise.all(jobs);
-    console.log(results);
-}
+        }, jobIdSelector, titleSelector, titleSelectorAlt, titleSelectorAlt2, companySelector);
 
-run();
+        // Expand job in new tab
+        const dropdownSelector = DROPDOWN_SEL.replace('INDEX', i);
+        const expandJobSelector = EXPAND_JOB_SEL.replace('INDEX', i);
+        const pageTarget = page.target();
+        await page.click(dropdownSelector);
+        await page.waitForSelector(expandJobSelector);
+        await page.click(expandJobSelector);
+        const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget);
+        const expPage = await newTarget.page();
+
+        // Get region, Job Summary, and Required Skills
+        const regionSel = '#postingDiv > div:nth-child(1) > div.panel-body > table > tbody > tr:nth-child(3) > td:nth-child(2)';
+        const jobSummarySel = '#postingDiv > div:nth-child(1) > div.panel-body > table > tbody > tr:nth-child(6) > td:nth-child(2)';
+        const reqSkillsSel = '#postingDiv > div:nth-child(1) > div.panel-body > table > tbody > tr:nth-child(8) > td:nth-child(2)';
+        await expPage.waitForSelector(regionSel);
+        const expObj = await expPage.evaluate((regSel, sumSel, skilSel) => {
+            const region = document.querySelector(regSel)
+                ? document.querySelector(regSel).innerHTML.trim()
+                : null;
+            const jobSummary = document.querySelector(sumSel)
+                ? document.querySelector(sumSel).innerHTML.trim()
+                : null;
+            const requiredSkills = document.querySelector(skilSel)
+                ? document.querySelector(skilSel).innerHTML.trim()
+                : null;
+            return {
+                region,
+                jobSummary,
+                requiredSkills,
+            };
+        }, regionSel, jobSummarySel, reqSkillsSel);
+        jobs.push({ ...jobObj, ...expObj, lastUpdated: new Date() });
+        await expPage.close();
+    }
+    return jobs;
+}
