a7697d909af63fad001b36df965a32e0a83332ae
Sam Yu <smyu03@gmail.com>
2020-01-15 01:10:35 -0500

    change proxy to package

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
-const express = require('express');const path = require('path');
+const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
@@ -12,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'localhost:3000'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
