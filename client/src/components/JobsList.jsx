6b6610a7c3b9d00d1affd8e9699cc04d3f6faae0
Sam Yu <smyu03@gmail.com>
2020-01-15 14:01:55 -0500

    table pagination

--- client/src/components/JobsList.jsx
+++ client/src/components/JobsList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core';
 import Table from '@material-ui/core/Table';
@@ -7,6 +7,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import Progress from '@material-ui/core/CircularProgress';
 
 const tableCols = [
@@ -16,11 +17,20 @@ const tableCols = [
 const styles = {
   tableContainer: {
     marginTop: 10,
-    maxHeight: 800,
+    maxHeight: '73vh',
   },
 };
 
 function JobsList({ classes, jobs, isFetching }) {
+  const rowsPerPage = 50;
+  const [page, setPage] = React.useState(0);
+  const tableRef = useRef();
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+    tableRef.current.scrollTop = 0;
+  };
+
   if (isFetching) {
     return (
       <div>
@@ -29,37 +39,47 @@ function JobsList({ classes, jobs, isFetching }) {
     );
   }
   return (
-    <TableContainer className={classes.tableContainer}>
-      <Table stickyHeader>
-        <TableHead>
-          <TableRow>
-            {tableCols.map((col) => <TableCell variant="head">{col}</TableCell>)}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {jobs.map((job) => (
+    <>
+      <TableContainer ref={tableRef} className={classes.tableContainer}>
+        <Table stickyHeader>
+          <TableHead>
             <TableRow>
-              <TableCell>{job.jobId}</TableCell>
-              <TableCell>{job.company}</TableCell>
-              <TableCell>{job.title}</TableCell>
-              <TableCell>{job.region}</TableCell>
-              <TableCell>{job.summary}</TableCell>
-              <TableCell>{job.skills}</TableCell>
-              <TableCell>{job.workTerm}</TableCell>
-              <TableCell>
-                <ul>
-                  {job.skillsList.map((skill) => (
-                    <li>
-                      {skill}
-                    </li>
-                  ))}
-                </ul>
-              </TableCell>
+              {tableCols.map((col) => <TableCell variant="head">{col}</TableCell>)}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {jobs.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((job) => (
+              <TableRow>
+                <TableCell>{job.jobId}</TableCell>
+                <TableCell>{job.company}</TableCell>
+                <TableCell>{job.title}</TableCell>
+                <TableCell>{job.region}</TableCell>
+                <TableCell>{job.summary}</TableCell>
+                <TableCell>{job.skills}</TableCell>
+                <TableCell>{job.workTerm}</TableCell>
+                <TableCell>
+                  <ul>
+                    {job.skillsList.map((skill) => (
+                      <li>
+                        {skill}
+                      </li>
+                    ))}
+                  </ul>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        component="div"
+        count={jobs.length}
+        rowsPerPage={rowsPerPage}
+        rowsPerPageOptions={[rowsPerPage]}
+        page={page}
+        onChangePage={handleChangePage}
+      />
+    </>
   );
 }
 
