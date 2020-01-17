31b3d1369da6e784b513573f6411ad2d574ec3e7
Sam Yu <smyu03@gmail.com>
2020-01-16 19:44:12 -0500

    Favicon and remove inactivive routes

--- server/routes.js
+++ server/routes.js
@@ -12,8 +12,8 @@ router.get('/current', auth.required, currentUser);
 router.get('/logout', auth.optional, logout);
 
 // Jobs Routes
-router.post('/fetchJobs', auth.required, fetchJobs);
+// router.post('/fetchJobs', auth.required, fetchJobs);
 router.get('/jobs', auth.required, getJobs);
-router.post('/jobs', auth.required, postJob);
+// router.post('/jobs', auth.required, postJob);
 
 module.exports = router;
